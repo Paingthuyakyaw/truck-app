d7b7b458d6e56dbf19d0562a3c1a9d208a5715f8
koheincs <heinhtetaungcu@gmail.com>
2026-05-09 23:58:37 +0630

    system refactor: - add generic types to useTranslation hook for better IDE support

--- hooks/use-translation.ts
+++ hooks/use-translation.ts
@@ -2,7 +2,7 @@
 import {useLocaleStore} from "@/stores/client/locale-store";
 import {translations,TranslationKeys} from "@/locale";
 
-export const useTranslation = (fileName:TranslationKeys) =>{
+export const useTranslation =<K extends TranslationKeys> (fileName:K) =>{
     const locale = useLocaleStore((state) => state.locale);
-    return translations[locale][fileName];
+    return translations[locale][fileName] as typeof translations['mm'][K];
 }
